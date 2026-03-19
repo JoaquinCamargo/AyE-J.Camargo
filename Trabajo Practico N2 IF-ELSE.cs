dffbc625d6531019c2a89d5b4cea2d3ea03471b9
JoaquinCamargo <joaquin.camargoet32@gmail.com>
2026-03-19 11:06:31 -0300

    Add files via upload

---
+++ Trabajo Practico N2 IF-ELSE.cs
@@ -0,0 +1,197 @@
+﻿Console.WriteLine("Coloque los numeros de abajo en orden y le diremos si es falso o no");
+
+Console.WriteLine("25>30");
+
+
+int a1;
+int a2;
+
+a1 = Convert.ToInt32(Console.ReadLine());
+a2 = Convert.ToInt32(Console.ReadLine());
+
+if (a1<a2) {
+    Console.WriteLine("Falso");
+
+}
+else {
+  
+    Console.WriteLine("Verdadero");
+}
+
+Console.WriteLine("14<=14");
+
+int b1;
+int b2;
+
+b1 = Convert.ToInt32(Console.ReadLine());
+b2 = Convert.ToInt32(Console.ReadLine());
+
+if (b1<=b2)
+{
+    Console.WriteLine("Verdadero");
+
+}
+else
+{
+
+    Console.WriteLine("Falso");
+}
+
+Console.WriteLine("0==0");
+
+
+int c1;
+int c2;
+
+c1 = Convert.ToInt32(Console.ReadLine());
+c2 = Convert.ToInt32(Console.ReadLine());
+
+if (c1==c2)
+{
+    Console.WriteLine("Falso");
+
+}
+else
+{
+
+    Console.WriteLine("Verdadero");
+}
+
+Console.WriteLine("10!=10");
+
+int d1;
+int d2;
+
+d1 = Convert.ToInt32(Console.ReadLine());
+d2 = Convert.ToInt32(Console.ReadLine());
+
+if (d1 != d2)
+{
+    Console.WriteLine("Verdadero");
+
+}
+else
+{
+
+    Console.WriteLine("Falso");
+}
+
+
+Console.WriteLine("8>=3");
+
+
+int e1;
+int e2;
+
+e1 = Convert.ToInt32(Console.ReadLine());
+e2 = Convert.ToInt32(Console.ReadLine());
+
+if (e1 >= e2)
+{
+    Console.WriteLine("Falso");
+
+}
+else
+{
+
+    Console.WriteLine("Verdadero");
+}
+
+Console.WriteLine("15<15");
+
+int f1;
+int f2;
+
+f1 = Convert.ToInt32(Console.ReadLine());
+f2 = Convert.ToInt32(Console.ReadLine());
+
+if (f1 < f2)
+{
+    Console.WriteLine("Falso");
+
+}
+else
+{
+
+    Console.WriteLine("Verdadero");
+}
+
+
+Console.WriteLine("100!=50");
+
+int g1;
+int g2;
+
+g1 = Convert.ToInt32(Console.ReadLine());
+g2 = Convert.ToInt32(Console.ReadLine());
+
+if (g1 != g2)
+{
+    Console.WriteLine("Verdadero");
+
+}
+else
+{
+
+    Console.WriteLine("Falso");
+}
+
+
+Console.WriteLine("7==7.0");
+
+int h1;
+int h2;
+
+h1 = Convert.ToInt32(Console.ReadLine());
+h2 = Convert.ToInt32(Console.ReadLine());
+
+if (h1 == h2)
+{
+    Console.WriteLine("Verdadero");
+
+}
+else
+{
+
+    Console.WriteLine("Falso");
+}
+
+
+Console.WriteLine("0.5>0");
+
+float i1;
+int i2;
+
+i1 = float.Parse(Console.ReadLine());
+i2 = Convert.ToInt32(Console.ReadLine());
+
+if (i1 > i2)
+{
+    Console.WriteLine("Falso");
+
+}
+else
+{
+
+    Console.WriteLine("Verdadero");
+}
+
+
+Console.WriteLine("-5>-2");
+
+int j1;
+int j2;
+
+j1 = Convert.ToInt32(Console.ReadLine());
+j2 = Convert.ToInt32(Console.ReadLine());
+
+if (j1 > j2)
+{
+    Console.WriteLine("Falso");
+
+}
+else
+{
+
+    Console.WriteLine("Verdadero");
+}
