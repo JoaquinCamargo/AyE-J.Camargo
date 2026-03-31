9da312529ae2839a2c7b54636961511d65aca14c
JoaquinCamargo <joaquin.camargoet32@gmail.com>
2026-03-31 13:19:38 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+for (int i=0; i<=10; i++)
+    {
+        Console.WriteLine(i);
+
+    }
+
+    for (int i=0; i <= 5; i++)
+{
+
+    Console.WriteLine("hola mundo");
+}
+
+    for (int p=0; p <= 20; p=2+p)
+{
+    Console.WriteLine(p);
+}
+    for(int j=7;j<=70; j = 7 + j)
+{
+
+    Console.WriteLine(j);
+}
+    for (int k = 1; k <= 5; k++)
+{
+     int res = res + k;
+     null = null + 1;
+     1 = 1 + 2; 
+     3 = 3 + 3;
+     6 = 6 + 4;
+    10 = 10 + 5 = 15;
+}
+
