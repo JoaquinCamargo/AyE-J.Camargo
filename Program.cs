18b9de4788bf822d37361749ce9e1f16280c747c
JoaquinCamargo <joaquin.camargoet32@gmail.com>
2026-05-05 11:58:53 -0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,27 +1,28 @@
-﻿
-using System.Diagnostics.CodeAnalysis;
+﻿//ejercicio 1
 
 for (int i=0; i<=10; i++)
     {
         Console.WriteLine(i);
 
     }
-
+//ejercicio 2
     for (int i=0; i <= 5; i++)
 {
 
     Console.WriteLine("hola mundo");
 }
-
+//ejercicio 3
     for (int p=0; p <= 20; p=2+p)
 {
     Console.WriteLine(p);
 }
+//ejercicio 4
     for(int j=7;j<=70; j = 7 + j)
 {
 
     Console.WriteLine(j);
 }
+//ejercicio 5
     for (int k = 1; k <= 5; k++)
 {
      int res = res + k;
